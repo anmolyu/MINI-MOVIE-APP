f2a28e439c97826d8c129fccee4904641c75e178
Anmol Mishra <anmolmishra2580@gmail.com>
2025-06-11 10:58:28 +0530

    Initial commit

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Debounce from "../hooks/Debounce";
+import "../styles/SearchBar.css";
+
+const SearchBar = ({ setMovies }) => {
+  const [query, setQuery] = useState("");
+
+  const debouncedQuery = Debounce(query, 500);
+
+  const fetchMovies = async (searchTerm) => {
+    if (!searchTerm) {
+      setMovies([]);
+      return;
+    }
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?s=${searchTerm}&apikey=4a3b711b`
+      );
+      const data = await response.json();
+      if (data.Search) {
+        setMovies(data.Search.slice(0, 10));
+      } else {
+        setMovies([]);
+      }
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setMovies([]);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchMovies(query);
+  };
+
+  React.useEffect(() => {
+    fetchMovies(debouncedQuery);
+  }, [debouncedQuery]);
+
+  return (
+    <form onSubmit={handleSubmit} className="search-bar">
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search for a movie..."
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+SearchBar.propTypes = {
+  setMovies: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
